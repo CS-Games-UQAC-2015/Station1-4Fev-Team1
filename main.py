08ae5b4fc30910de4d97c8cf7dce65ee464d0868
Alex <alex3521@hotmail.fr>
2015-02-05 02:13:45 +0000

    Commit Alexis

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 #taper "python main.py" dans le Power Shell pour runner
 #!/usr/bin/python
 
+# WHAT IS DONE :
+#   OUTPUT et INPUT
+
+# WHAT TO BE DONE : 
+#   LOGIC
+
 
 #Input
-file = open("A-small-practice.in", encoding='utf-8')
+file = open("A-small-practice.in")
 T = int(file.readline())#Number of cases
 
 outlets = []
-for i in range(1, T): #Go through all cases
+flows = []
 
-	N = file.read(0) #Number of outlets
-	L =  0#Length of current in outlets
-	outlets.append([])
-	print(N)
+for i in range(1, T+1): #Go through all cases
+
+	N, L= file.readline().split(" ") #Number of outlets
+	# Cast N et L en int
+	N = int(N) 
+	L = int(L)
+	
+	outlets = file.readline().split(" ") # Table of all N Outlets
+	flows = file.readline().split(" ") # Table of all N Flows
+	
+	#LOGIC GOES HERE :
+    #use N, L, outlets and flows variable to find algorithm
+	
+	
+	#Output
+	y = 0 #minimum number of switches to charge all devices
+	# If not possible, y must equal : "NOT POSSIBLE"
+	print ("Case #" + str(i) + ": " + str(y)) 
 
 
 file.close()
 
 
-#Output
-x = 1 #case number
-y = 0 #minimum number of switches to charge all devices
-print ("Case #" + str(x) + ": " + str(y))
