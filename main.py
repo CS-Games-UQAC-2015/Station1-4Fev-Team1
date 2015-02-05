b61f82ed6fdd63a949f78c6ba7229f185cd37b12
JohnnyBoy <johnnyguerin@outlook.com>
2015-02-04 22:01:48 -0500

    Fin tour 3 (johnny)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#taper "python main.py" dans le Power Shell pour runner
+﻿#taper "python main.py" dans le Power Shell pour runner
 #!/usr/bin/python
 
 # WHAT IS DONE :
@@ -17,24 +17,46 @@ flows = []
 
 for i in range(1, T+1): #Go through all cases
 
-	N, L= file.readline().split(" ") #Number of outlets
-	# Cast N et L en int
-	N = int(N) 
-	L = int(L)
+ N, L= file.readline().split(" ") #Number of outlets
+ # Cast N et L en int
+ N = int(N)
+ L = int(L)
 	
-	outlets = file.readline().split(" ") # Table of all N Outlets
-	flows = file.readline().split(" ") # Table of all N Flows
+ outlets = file.readline().split(" ") # Table of all N Outlets
+ flows = file.readline().split(" ") # Table of all N Flows
 	
 	#LOGIC GOES HERE :
     #use N, L, outlets and flows variable to find algorithm
-	
-	
+ solutionFind = False
+ #continuefor = True
+ outlets.sort()
+ flows.sort()
+
+ #for it1 in range(1, N+1):
+  #if outlets[it1]==flows[it1]:
+   #continuefor = True
+  #else:
+  # continuefor=False
+   #break
+
+
+
+
+
+
+
 	#Output
-	y = 0 #minimum number of switches to charge all devices
+ y = 0 #minimum number of switches to charge all devices
 	# If not possible, y must equal : "NOT POSSIBLE"
-	print ("Case #" + str(i) + ": " + str(y)) 
+ if solutionFind:
+  print ("Case #" + str(i) + ": " + str(y))
+ else:
+  print ("Case #" + str(i) + ": " + "NOT POSSIBLE")
+
+ print (" Scusez les gars j'ai essayer d'hardcoder pleins de truc pi jconnais pas le langage faique allez toute chier on aura failer cette épreuve la !!")
 
 
 file.close()
+
 
 
