1bf8a856360532266a7d983bcea4d991150d5799
Gab <ghebert10@gmail.com>
2015-02-04 20:33:22 -0500

    aasdsd

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 #taper "python main.py" dans le Power Shell pour runner
+#!/usr/bin/python
 
-print ("test")
+
+#Input
+file = open("A-small-practice.in", encoding='utf-8')
+T = int(file.readline())#Number of cases
+
+outlets = []
+for i in range(1, T): #Go through all cases
+
+	N = file.read(0) #Number of outlets
+	L =  0#Length of current in outlets
+	outlets.append([])
+	print(N)
+
+
+file.close()
+
+
+#Output
+x = 1 #case number
+y = 0 #minimum number of switches to charge all devices
+print ("Case #" + str(x) + ": " + str(y))
